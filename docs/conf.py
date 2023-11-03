a434fe07fc1de2b25338c29380b72b334bae7710
jeffquinn-msk <quinnj2@mskcc.org>
2023-11-03 15:13:31 -0400

    fix doc build

--- docs/conf.py
+++ docs/conf.py
@@ -54,4 +54,4 @@ html_theme = "alabaster"
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ["_static"]
 
-autodoc_mock_imports = ["numpy", "pandas", "scipy", "sklearn", "joblib", "omegaconf"]
+autodoc_mock_imports = ["numpy", "pandas", "scipy", "tqdm", "h5py"]
