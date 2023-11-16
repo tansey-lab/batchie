f80d53ae51a4a6abb12adde93343646b904903c6
jeffquinn-msk <quinnj2@mskcc.org>
2023-11-16 16:55:24 -0500

    add perf tesT

--- src/batchie/common.py
+++ src/batchie/common.py
@@ -9,14 +9,9 @@ CONTROL_SENTINEL_VALUE = -1
 def copy_array_with_control_treatments_set_to_zero(
     arr: ArrayType, treatment_array: ArrayType
 ):
-    results = []
-    for idx in treatment_array:
-        if idx != CONTROL_SENTINEL_VALUE:
-            results.append(arr[idx])
-        else:
-            results.append(np.zeros_like(arr[0]))
-
-    return np.array(results)
+    results = arr[treatment_array, ...]
+    results[treatment_array == CONTROL_SENTINEL_VALUE, ...] = 0.0
+    return results
 
 
 N_UNIQUE_TREATMENTS = "n_unique_treatments"
